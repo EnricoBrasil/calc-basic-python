dc8b3fd06bdc373bc39c934d64cde72c7b3d85d9
EnricoBrasil <cylodabiliro@hotmail.com>
2023-09-28 16:18:08 -0300

    develop:changes title

--- index.py
+++ index.py
@@ -3,7 +3,11 @@ from operations.divisao import divisao as div
 from operations.subtracao import subtracao as sub 
 from operations.multiplicacao import multiplicacao as mult
 
+print("SOMA")
 sm()
+print("SUBTRAÇÃO")
 sub()
+print("DIVISÃO")
 div()
+print("MULTIPLICAÇÃO")
 mult()
